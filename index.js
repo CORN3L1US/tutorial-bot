a7a9af9629001f1b6954287a5b5bf6b8dc71e7e9
Cornelius <75083253+CORN3L1US@users.noreply.github.com>
2021-01-07 15:38:43 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,95 @@
+/*
+──────────────────────────────────────────────────────────────────────────────────────────────
+--██████████-███████████-████████--███████---██-███████--██------████-██-----██--████████-----
+--███--------███-----███-██----███-██---██---██-------██-██-----██-██-██-----██--██-----██----
+--██---------██-------██-██----██--██---██--███--------█-██--------██-██-----██--██-----------
+--██---------██-------██-███████---██---██--██------████-██--------██-██-----██----███--------
+--███--------███-----███-██---███--██---█████--██------█-███-------██-███----██-██----███-----
+--██████████-███████████-███--████-██---████----████████-█████████-██-████████---████████-----
+────────────────────────────────────────────────────────────────────────────────────────────── 
+ Tutorial Discord Bot by CORN3L1US 
+ Discord: CORN3L1US#9999
+ Feel free to use it as you wish :)
+
+
+ Wanna upgrade the bot? Visit this website to learn Discord.js: https://discord.js.org/#/
+
+ Wanna learn Javascript? Visit this website for tips and tricks: https://www.w3schools.com/js/DEFAULT.asp
+
+*/
+
+
+// Just a bunch of necessary variables for the bot to work properly:
+
+const Discord = require("discord.js"); // Variable that defines the Discord.js framework
+
+var bot = new Discord.Client(); // Defines a new discord client (A bot).
+
+const token = "TOKEN HERE"; // copypaste Your token here. Get your token here: https://discord.com/developers/applications
+
+
+const prefix = "PREFIX HERE";  // Choose your prefix here
+
+
+
+bot.on("ready", () => {
+
+ console.log(`${bot.user.tag} is up and running!`); // A function that makes you sure that your bot is online
+
+
+
+
+
+  bot.user.setPresence({ // A function that gives your bot an custom status on discord
+
+    activity: { name: `I'm wating for a status`, type: "WATCHING" }, // Choose: WATCHING, LISTENING or PLAYING for the status
+
+    status: "online",
+
+  });
+
+});
+
+
+// A basic command handler for your commands. 
+//IF you know what you are doing, then you can make a more advanced and manageable command handler with multiple files.
+bot.on("message", (message) => {
+
+  if(!message.content.startsWith(prefix) || message.author.bot) return; // Basically just tells the bot that if your command dose not start with your prefix, then it will not work.
+                            
+  const args = message.content.slice(prefix.length).split(/ +/); // Makes us able to make commands with gaps
+
+  const command = args.shift().toLowerCase();
+
+  if(command === 'hey'){ // An if statemant. So if you write "!HEY" it will reply with "Whats Up!"."
+    message.reply("whats up!") // replys
+    
+  } else if (command === 'multiple') { // Make multiple commands with an else if statement. Works the same way as the command above.
+    message.channel.send("Yes, multiple :O") // Sends a message to the channel instead of replying
+
+  } else if (command === 'embed') { // An embed is a box with text formed in a really nice way.
+
+    // You can change a lot of different things in embeds, to match your style
+
+    const embed = new Discord.MessageEmbed() // A local variable that defines an embed
+
+	.setColor('#00ff00') // An color for your embed. right now it's green. you can find more colors here: https://www.w3schools.com/colors/colors_picker.asp
+	.setTitle(`I'm a title`) // A title for your embed
+	.setDescription(`I'm a description`) // A description for your embed
+	.setThumbnail('https://cdn.discordapp.com/attachments/752934115791339615/796741239055581184/corn2.png') // A little picture for your embed
+	.addField('Inline field title', 'Some value here', true) // Makes a filed with text. You can ad more if you wan't to.
+	.setTimestamp() // Sets a timestamp in your embed.
+
+// This was just a few features for an embed. you find alot more information about embeds here: https://discordjs.guide/popular-topics/embeds.html#using-the-richembedmessageembed-constructor
+
+    message.channel.send(embed) // Sends the variable "embed"
+  } 
+
+});
+
+
+
+
+
+
+bot.login(token); // Logs your bot in with your token
---
+++ readme.md
@@ -0,0 +1,52 @@
+──────────────────────────────────────────────────────────────────────────────────────────────
+--██████████-███████████-████████--███████---██-███████--██------████-██-----██--████████-----
+--███--------███-----███-██----███-██---██---██-------██-██-----██-██-██-----██--██-----██----
+--██---------██-------██-██----██--██---██--███--------█-██--------██-██-----██--██-----------
+--██---------██-------██-███████---██---██--██------████-██--------██-██-----██----███--------
+--███--------███-----███-██---███--██---█████--██------█-███-------██-███----██-██----███-----
+--██████████-███████████-███--████-██---████----████████-█████████-██-████████---████████-----
+────────────────────────────────────────────────────────────────────────────────────────────── 
+
+Welcome to my tutorial Discord Bot!
+
+In this bot I will explain to you the basics of a Discord Bot in the framework "Discord.js".
+
+It's free to use and you are allowed to do what ever you wan't with the bot. You are also allowed to remove watermarks or what ever you like.
+
+I do not provide support on the bot in any way.
+
+Follow my github if you liked it. :)
+
+Regards Cornelius <3
+
+
+
+Start the bot:
+
+Step 1. I really recommend downloading Visual Studio Code for codeing Discord Bots. It's not necessary.
+
+Step 2. dowload node.js: https://nodejs.org/en/
+
+Step 6. Download node_modules and package-lock by choosing your folder and then typing "npm install discord.js" in your Visual Studio Code Terminal or your node.js command promt as you downloaded before.
+
+Step 3. Make a bot application and get a token here: https://discord.com/developers/applications
+
+Step 4. Invite your Bot to your Discord Server by copying your client id and changeing permissions here: https://discordapi.com/permissions.html
+
+Step 5. Copypaste your token into the index file.
+
+Step 8. Press the batchfile in the folder.
+
+If you have done the steps correctly, your bot will now be up and running and ready to develop on.
+
+
+
+
+// Wanna upgrade the bot? Visit this website to learn Discord.js: https://discord.js.org/#/
+
+// Wanna learn Javascript? Visit this website for tips and tricks: https://www.w3schools.com/js/DEFAULT.asp
+
+
+
+
+
